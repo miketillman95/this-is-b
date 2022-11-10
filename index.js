2856c7cf17b3b1f4b139dfe8fb587eb97a29aada
mike <matillman95@gmail.com>
2022-11-10 14:15:46 -0700

    add web config

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const server = express()
+const cors = require('cors')
+
+
+server.use(express.json())
+server.use(cors())
+
 server.get('/', (req,res) => res.json({message:'This better work'}))
 server_port = process.env.PORT || 8000
 server.listen( server_port, () => {
